05e0e1d97663f16bca873dcb0055eb0ad4e36762
SkylineDriftin <DavidkimAE86@gmail.com>
2025-11-03 20:19:06 -0800

    Implemented sensor reading for imu/magnetometer via i2c. outputs data in both vector3 form and adafruit unified sensor event.

--- controls/sensors/src/main.cpp
+++ controls/sensors/src/main.cpp
@@ -9,11 +9,10 @@
 
 
 
-// IMU INITIALIZATION
-
+// IMU INITIALIZATION (I2C)
 // Check I2C device address and correct line below (by default address is 0x29 or 0x28)
-//                                   id, address
-Adafruit_BNO055 bno = Adafruit_BNO055(-1, 0x28, &Wire);
+//                                    id, address
+Adafruit_BNO055 bno = Adafruit_BNO055(-1,  0x28, &Wire);
 
 // ############ PUT REST OF SENSOR INITALIZATION HERE ################
 
@@ -24,7 +23,7 @@ void setup(void)
 {
   //serial initialization
   Serial.begin(115200);
-  while (!Serial) delay(10);  // wait for serial port to open!
+  while (!Serial) delay(10);  // wait for serial port to initialize
 
 
   //IMU initialization
@@ -37,66 +36,61 @@ void setup(void)
   
   //IMU integrated temp resadings 
   int8_t temp = bno.getTemp();
+  delay(1000);
+  bno.setExtCrystalUse(true); //note: board may not include external crystal. External crystal for timing is better 
+
+
+
 
   //############ PUT REST OF SENSOR INTIALIZATION HERE
 
 
-  delay(1000);
-
-
-
-  bno.setExtCrystalUse(true);
 
   Serial.println("Calibration status values: 0=uncalibrated, 3=fully calibrated");
 }
 
 void loop(void)
 {
-  // IMU 
-  // Possible vector values can be:
-  // - VECTOR_ACCELEROMETER - m/s^2
-  // - VECTOR_MAGNETOMETER  - uT
-  // - VECTOR_GYROSCOPE     - rad/s
-  // - VECTOR_EULER         - degrees
-  // - VECTOR_LINEARACCEL   - m/s^2
-  // - VECTOR_GRAVITY       - m/s^2
-  imu::Vector<3> euler = bno.getVector(Adafruit_BNO055::VECTOR_EULER);
 
-  /* Display the floating point data */
-  Serial.print("X: ");
-  Serial.print(euler.x());
-  Serial.print(" Y: ");
-  Serial.print(euler.y());
-  Serial.print(" Z: ");
-  Serial.print(euler.z());
-  Serial.print("\t\t");
+  // IMU READING
+  // Retrieves all data from IMU chip in two different formats:
 
-  /*
-  // Quaternion data
+  //in the adafruit unified sensor format.
+  //declare sensor events
+  sensors_event_t orientationData, angVelocityData, linearAccelData;
+  sensors_event_t magnetometerData, accelerometerData, gravityData;
+
+  // Get all the available sensor events
+  bno.getEvent(&orientationData, Adafruit_BNO055::VECTOR_EULER);                    // degrees
+  bno.getEvent(&angVelocityData, Adafruit_BNO055::VECTOR_GYROSCOPE);                // rad/s
+  bno.getEvent(&linearAccelData, Adafruit_BNO055::VECTOR_LINEARACCEL);              // m/s^2
+  bno.getEvent(&magnetometerData, Adafruit_BNO055::VECTOR_MAGNETOMETER);            // µT
+  bno.getEvent(&accelerometerData, Adafruit_BNO055::VECTOR_ACCELEROMETER);          // m/s^2
+  bno.getEvent(&gravityData, Adafruit_BNO055::VECTOR_GRAVITY);                      // m/s^2
+
+  //in vector3 form using imumaths lib
+  // --- Read vector data (3D) ---
+  imu::Vector<3> euler     = bno.getVector(Adafruit_BNO055::VECTOR_EULER);          // degrees
+  imu::Vector<3> gyro      = bno.getVector(Adafruit_BNO055::VECTOR_GYROSCOPE);      // rad/s
+  imu::Vector<3> accel     = bno.getVector(Adafruit_BNO055::VECTOR_ACCELEROMETER);  // m/s^2
+  imu::Vector<3> linAccel  = bno.getVector(Adafruit_BNO055::VECTOR_LINEARACCEL);    // m/s^2
+  imu::Vector<3> mag       = bno.getVector(Adafruit_BNO055::VECTOR_MAGNETOMETER);   // µT
+  imu::Vector<3> gravity   = bno.getVector(Adafruit_BNO055::VECTOR_GRAVITY);        // m/s^2
+
+  // Quaternion -- same as vector3 euler or sensor_event orientationData, just in quaternion form instead
   imu::Quaternion quat = bno.getQuat();
-  Serial.print("qW: ");
-  Serial.print(quat.w(), 4);
-  Serial.print(" qX: ");
-  Serial.print(quat.x(), 4);
-  Serial.print(" qY: ");
-  Serial.print(quat.y(), 4);
-  Serial.print(" qZ: ");
-  Serial.print(quat.z(), 4);
-  Serial.print("\t\t");
-  */
 
-  /* Display calibration status for each sensor. */
-  uint8_t system, gyro, accel, mag = 0;
-  bno.getCalibration(&system, &gyro, &accel, &mag);
-  Serial.print("CALIBRATION: Sys=");
-  Serial.print(system, DEC);
-  Serial.print(" Gyro=");
-  Serial.print(gyro, DEC);
-  Serial.print(" Accel=");
-  Serial.print(accel, DEC);
-  Serial.print(" Mag=");
-  Serial.println(mag, DEC);
+  // Temperature (internal temperature of onbaord die, in celsius. shouldnt be more than 40 deg probably. check datasheet)
+  int8_t temp = bno.getTemp();
 
+  // Calibration status 
+  // 0 is uncalibrated, 3 is fully callibrated. gets more calibrated when you move it around more. 
+  uint8_t systemCal, gyroCal, accelCal, magCal = 0;
+  bno.getCalibration(&systemCal, &gyroCal, &accelCal, &magCal);
+
+  // ################# PUT REST OF SENSOR LOOP STUFF HERE ###################
+
+  
   delay(SAMPLERATE_DELAY_MS);
 
 
